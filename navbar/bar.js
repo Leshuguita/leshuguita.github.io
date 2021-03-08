24f0b0d8da7748fa97398e4c2049610f925752a4
Leshuguita <ian.armstrong.dendal@gmail.com>
2021-03-08 19:33:13 -0300

    bar height fix??

--- navbar/bar.js
+++ navbar/bar.js
@@ -2,6 +2,6 @@ var navbarIframe = window.frameElement;
 resizeIFrame();
 
 function resizeIFrame() {
-	//navbarIframe.width  = navbarIframe.contentWindow.document.body.scrollWidth;
 	navbarIframe.height = navbarIframe.contentWindow.document.body.scrollHeight+4;
+	navbarIframe.style.height = navbarIframe.height + "px";
 }
